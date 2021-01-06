6f442604dbf3cd55e6e9e9016a8b6e254e732b76
MatheuSilvSant <matheu.lpp@gmail.com>
2021-01-06 13:24:48 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Hello world</h1> 
+      <h2>Hello !!</h2>     
+    </div>
+  );
+}
+
+export default App;
