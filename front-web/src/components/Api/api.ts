241c170be603bb309e41250e31c1327e3a341a65
MatheuSilvSant <matheu.lpp@gmail.com>
2021-01-09 03:34:03 -0300

    Add env vars

--- front-web/src/components/Api/api.ts
+++ front-web/src/components/Api/api.ts
@@ -1,7 +1,7 @@
 import { OrderPayload } from './../../interface/types';
 import axios from "axios";
 
-const API_URL = 'https://sds2-matheus.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
