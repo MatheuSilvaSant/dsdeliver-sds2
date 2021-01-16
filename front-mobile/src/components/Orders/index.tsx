93cbf9d0c023d70b3b1a8b1572e488474c5cbb58
MatheuSilvSant <matheu.lpp@gmail.com>
2021-01-16 15:36:20 -0300

    Finish Front-mobile

--- front-mobile/src/components/Orders/index.tsx
+++ front-mobile/src/components/Orders/index.tsx
@@ -1,18 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View,ScrollView} from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View,ScrollView, Alert} from 'react-native';
+import { TouchableNativeFeedback } from 'react-native-gesture-handler';
+import { fetchOrders} from '../../api/api';
+import { Order } from '../../interfaces/types';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
 
 export default function Orders() {
+
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+  
+  const fetchData = () =>{
+    setIsLoading(true);
+    fetchOrders()
+      .then(response => setOrders(response.data))
+      .catch(error =>  Alert.alert('Houve um erro ao buscar os pedidos'))
+      .finally(()=> setIsLoading(false));
+  }
+
+  useEffect(()=>{
+   if(isFocused){
+     fetchData();
+   }
+  },[isFocused]);
+
+const handlerOnPress = (order: Order) =>{
+      navigation.navigate('OrderDetails',{
+        order
+      })
+}
+
+
   return (
    <>
     <Header/>
     <ScrollView  style={styles.container} >
-        <OrderCard/>
-        <OrderCard/>
-        <OrderCard/>
-        <OrderCard/>
-        <OrderCard/>
+       {isLoading ? (
+         <Text>Buscando pedido...</Text>
+       ) : (orders.map(order=>(
+          <TouchableNativeFeedback 
+          key={order.id} 
+          onPress={() => handlerOnPress(order)}
+          
+          >
+            <OrderCard order={order} />
+          </TouchableNativeFeedback>
+      )))}
     </ScrollView>
    </>
   );
